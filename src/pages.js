c62ea5aa0eb81db0d87134bdb67ef78aa96ea3b1
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-06-29 23:30:01 -0300

    cluster need variables in a object

--- src/pages.js
+++ src/pages.js
@@ -11,26 +11,23 @@ const URL = `https://www.magazineluiza.com.br/busca/geladeira/`;
 
     await page.waitForSelector('[data-testid="product-list"]');
 
-    const elements = await page.$$eval('[data-testid="product-card-container"]', (nodes) => {
+    const models = await page.$$eval('[data-testid="product-card-container"]', (nodes) => {
         const regex = /\b[A-Z]\w*\d\w*\b/;
 
-        return nodes.map((node) => {
+       return nodes.map((node) => {
             const nameElement = node.querySelector('[data-testid="product-title"]');
             const modelElement = node.querySelector('[data-testid="review"]');
             const priceElement = node.querySelector('[data-testid="price-value"]');
 
             const name = nameElement ? nameElement.innerText : '';
-            const model = modelElement ? modelElement.innerText : '';
             const price = priceElement ? priceElement.innerText : '';
-
-            return name.match(regex)
-
-            // return { name, model, price };
+            const model =  name.match(regex)
+            return { name, model, price };
         });
     });
 
-    elements.forEach((element) => {
-       console.log(element)
+    models.forEach((element) => {
+        console.log(element)
     });
 
 
